acb63c55619663607a7d4b7458f6a9bf1568726c
Tahir Javaid <tahirjavaid.chep@gmail.com>
2022-02-16 09:09:57 +0100

    DDXS code

--- code/collectInputs.sh
+++
@@ -1,3 +0,0 @@
-python -u collectInputs.py --obsName="massZ1" --obsName2="massZ2" --obsBins2="|12|24|" 
-python -u collectInputs.py --obsName="massZ1" --obsName2="massZ2" --obsBins2="|24|32|" 
-python -u collectInputs.py --obsName="massZ1" --obsName2="massZ2" --obsBins2="|32|65|" 
--- code/doAllObs_DXS.sh
+++
@@ -1,6 +0,0 @@
-nohup python -u runHZZFiducialXS.py --dir="root://cmsio5.rc.ufl.edu//store/user/t2/users/klo/Higgs/HZZ4l/NTuple/Run2/MC80X_M17_4l_Feb21/" --obsName="massZ1" --obsBins="|50.0|74.0|88.0|94.0|106.0|" --obsName2="massZ2" --obsBins2="|12|24|"  --calcSys  >& log_massZ1vsmassZ2_12-24_Run2Fid.txt &
-nohup python -u runHZZFiducialXS.py --dir="root://cmsio5.rc.ufl.edu//store/user/t2/users/klo/Higgs/HZZ4l/NTuple/Run2/MC80X_M17_4l_Feb21/" --obsName="massZ1" --obsBins="|50.0|74.0|88.0|94.0|106.0|" --obsName2="massZ2" --obsBins2="|24|32|"  --calcSys  >& log_massZ1vsmassZ2_24-32_Run2Fid.txt &
-nohup python -u runHZZFiducialXS.py --dir="root://cmsio5.rc.ufl.edu//store/user/t2/users/klo/Higgs/HZZ4l/NTuple/Run2/MC80X_M17_4l_Feb21/" --obsName="massZ1" --obsBins="|50.0|74.0|88.0|106.0|" --obsName2="massZ2" --obsBins2="|32|65|"  --calcSys  >& log_massZ1vsmassZ2_32-65_Run2Fid.txt &
-
-
-
